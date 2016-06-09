62dbd26e8f29a6554ac5bb9f937d4087a20f8af3
Prasheel Soni <prasheelsoni11@gmail.com>
2016-06-09 20:40:26 +0530

    Final Commit

---
+++ app/src/main/java/tk/prasheelsoni/stockhawk/touch_helper/OnStartDragListener.java
@@ -0,0 +1,13 @@
+package tk.prasheelsoni.stockhawk.touch_helper;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by sam_chordas on 10/6/15.
+ * credit to Paul Burke (ipaulpro)
+ */
+public interface OnStartDragListener {
+
+  void onStartDrag(RecyclerView.ViewHolder viewHolder);
+
+}
